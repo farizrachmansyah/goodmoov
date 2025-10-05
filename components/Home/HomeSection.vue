0704f00bdea560a4966063fb702790f0ab2b8c80
Fariz Rachmansyah <farizrachmansyah7@gmail.com>
2025-10-05 21:09:08 +0700

    feat: create section title component

--- components/Home/HomeSection.vue
+++ components/Home/HomeSection.vue
@@ -1,9 +1,6 @@
 <template>
   <section>
-    <div class="section-title relative w-fit min-w-10 mb-2 pt-[2px]">
-      <h1 class="font-semibold text-white">{{ props.title }}</h1>
-    </div>
-
+    <SectionTitle :title="props.title" />
     <section class="overflow-x-auto scrollbar-hide">
       <ul class="flex gap-2 w-full">
         <li v-for="item in 10" :key="item" class="max-w-[160px]">
---
+++ components/SectionTitle.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    :class="[
+      'relative w-fit min-w-10 mb-2 pt-[2px] before:content-[\'\'] before:absolute before:top-0 before:w-full before:max-w-10 before:h-[2px] before:bg-primary',
+      props.isRightAlign ? 'before:right-0' : 'before:left-0'
+    ]"
+  >
+    <h1 :class="`font-semibold text-white text-${props.titleSize}`">{{ props.title }}</h1>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    default: 'Title'
+  },
+  titleSize: {
+    type: String,
+    default: 'base'
+  },
+  isRightAlign: {
+    type: Boolean,
+    default: false
+  }
+});
+</script>
